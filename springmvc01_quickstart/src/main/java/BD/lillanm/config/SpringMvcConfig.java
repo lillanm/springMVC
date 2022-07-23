8fa1a2d140d4791e6593022334f459f16c8362a3
lillanm <2408203905@qq.com>
2022-07-23 23:56:22 +0800

    springmvc_quickstart

---
+++ springmvc01_quickstart/src/main/java/BD/lillanm/config/SpringMvcConfig.java
@@ -0,0 +1,9 @@
+package BD.lillanm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("BD.lillanm.controller")
+public class SpringMvcConfig {
+}
